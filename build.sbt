672921149d5620db0b63d7da0809f72fd4fca38c
Prashant <chaudharyprashant105@gmail.com>
2023-03-26 11:40:01 +0530

    map(),reduce(),zip(),parallelize()

--- build.sbt
+++ build.sbt
@@ -1,5 +1,6 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "2.13.10"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.3.2"
 lazy val root = (project in file("."))
   .settings(
     name := "ScalaImplement"
--- src/main/scala/functional_combinators.scala
+++ src/main/scala/functional_combinators.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.{SparkConf, SparkContext}
 object functional_combinators extends App{
   // map()
   val numbers = List(1, 2, 3, 4)
@@ -17,4 +18,16 @@ object functional_combinators extends App{
   val num3 = num2.fold(0)((m: Int, n: Int) => m + n)
   println(num3)
   // parallelize()
+
+  val conf = new SparkConf().setAppName("MyApp").setMaster("local[*]")
+  val sc = new SparkContext(conf)
+
+  val data = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val rdd = sc.parallelize(data)
+
+  val sum = rdd.reduce(_ + _)
+  println("Sum: " + sum)
+
+  sc.stop()
+
 }
